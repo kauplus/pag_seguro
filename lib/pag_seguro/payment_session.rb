20ba60be1e1c26d782ca9b6941ca8d964e0cbc84
Gustavo Caixeta <gustavo.caixeta@kauplus.com.br>
2013-04-24 13:50:51 -0300

    Insert a better error handling to the payment session creation

--- lib/pag_seguro/payment_session.rb
+++ lib/pag_seguro/payment_session.rb
@@ -8,8 +8,25 @@ module PagSeguro
     end
 
     def init_session
-      session = RestClient.post(session_url_without_params, {:token => @token, :email => @email})
-      @id = Nokogiri::XML(session).search('//session').at('id').text
+      @response ||= parse_payment_session_response
+      parse_id
+    end
+
+    def parse_payment_session_response
+      response = RestClient.post(session_url_without_params, {:token => @token, :email => @email}) { |resp, request, result| resp }
+      if response.code == 200
+        response.body
+      elsif response.code == 401
+        raise Errors::Unauthorized
+      elsif response.code == 400
+        raise Errors::InvalidData.new(response.body)
+      else
+        raise Errors::UnknownError.new(response)
+      end
+    end
+
+    def parse_id
+      @id = Nokogiri::XML(@response.body).search('//session').at('id').text
     end
 
     def session_url_without_params
